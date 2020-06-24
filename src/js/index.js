58c428ee7b351181ebbc3b82becf27972519396c
hermansemykozov <semikozovgerman@gmail.com>
2020-06-24 19:29:02 +0300

    add custom class on btn touch - js

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,17 @@ jQuery(function () {
 
 document.addEventListener("DOMContentLoaded", () => {
   const openPanelBtn = document.getElementById("open-panel");
+  const btns = document.querySelectorAll(".btn");
 
-  openPanelBtn.addEventListener("click", () => {
+  openPanelBtn && openPanelBtn.addEventListener("click", () => {
     document.getElementById("nav-panel").classList.remove("hidden");
     openPanelBtn.remove();
   });
+
+  btns.forEach(btn => {
+    btn.addEventListener("touchstart", event => btn.classList.add("btn--pressed"));
+    btn.addEventListener("touchend touchcancel", event => btn.classList.remove("btn--pressed"));
+  })
 });
 
 // svg sprites
