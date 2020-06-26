502cfc0d853f04f77cf2cdb010faeaa3c8c26b95
hermansemykozov <semikozovgerman@gmail.com>
2020-06-26 09:57:47 +0300

    refactor meeting room

---
+++ imnij-dom/imnij-dom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  imnij-dom
+//
+//  Created by Herman Semykozov on 26.06.2020.
+//  Copyright © 2020 Herman Semykozov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
