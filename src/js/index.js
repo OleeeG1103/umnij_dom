4b90465a85626c083fd3adbcb158b4a643713546
hermansemykozov <semikozovgerman@gmail.com>
2020-07-05 16:20:04 +0300

    CHANGE: js displaying functionality for 1-2 screens

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,94 @@
-import jQuery from "jquery";
-
-jQuery(function () {
-  // твой код
-});
-
 document.addEventListener("DOMContentLoaded", () => {
-  const openPanelBtn = document.getElementById("open-panel");
+  const $topBtnBackContainer = document.getElementById("back-btn-container");
+  const $topBtnBack = document.getElementById("back-btn");
+  const $tvNav = document.getElementById("demonstration-tv");
+  const $channelsMenu = document.querySelector(".channels-menu");
+  const enablePanelBtn = document.getElementById("enable-panel");
+  const $demonstrationTV = document.getElementById("demonstration-tv");
+  const $appleTVMenuItem = document.getElementById("apple-tv");
+  const $BKCMenuItem = document.getElementById("bkc");
+  const $TVMenuItem = document.getElementById("tv");
+  const $appleTVMenuItemBtn = $appleTVMenuItem.querySelector(".btn");
+  const $BKCMenuItemBtn = $BKCMenuItem.querySelector(".btn");
+  const $TVMenuItemBtn = $TVMenuItem.querySelector(".btn");
+  const $appleTVoptions = $appleTVMenuItem.querySelectorAll(".menu-bkc__item");
+  const $BKCoptions = $BKCMenuItem.querySelectorAll(".menu-bkc__item");
+  const $TVoptions = $TVMenuItem.querySelectorAll(".menu-bkc__item");
+  const $openChannelsBtn = document.getElementById("open-channels-btn");
   const btns = document.querySelectorAll(".btn");
+  const $tooltips = document.querySelectorAll(".tooltip");
 
-  openPanelBtn && openPanelBtn.addEventListener("click", () => {
-    document.getElementById("nav-panel").classList.remove("hidden");
-    openPanelBtn.remove();
+  // prevent hiding on click
+  $tooltips.forEach(tooltip => tooltip.addEventListener("click", e => e.stopPropagation()));
+
+  enablePanelBtn && enablePanelBtn.addEventListener("click", () => {
+    enablePanelBtn.remove();
+    $demonstrationTV.classList.remove("d-none");
   });
 
+  // touch
   btns.forEach(btn => {
     btn.addEventListener("touchstart", event => btn.classList.add("btn--pressed"));
     btn.addEventListener("touchmove", event => btn.classList.contains("btn--pressed") && btn.classList.remove("btn--pressed"));
     btn.addEventListener("touchend", event => btn.classList.contains("btn--pressed") && btn.classList.remove("btn--pressed"));
     btn.addEventListener("touchcancel", event => btn.classList.contains("btn--pressed") && btn.classList.remove("btn--pressed"));
+  });
+
+  handleMenuOptionsClick($appleTVoptions);
+  handleMenuOptionsClick($BKCoptions);
+
+  toggleMenuTooltip($appleTVMenuItemBtn);
+  toggleMenuTooltip($BKCMenuItemBtn);
+  toggleMenuTooltip($TVMenuItemBtn);
+
+  $openChannelsBtn.addEventListener("click", () => {
+    channelViewSwither($tvNav, $channelsMenu);
+    $topBtnBackContainer.classList.add("d-block");
+  });
+
+  $topBtnBack.addEventListener("click", () => {
+    handleBack($tvNav, $channelsMenu);
+    $topBtnBackContainer.classList.remove("d-block");
   })
+
 });
+
+function handleBack(prev, cur) {
+  cur.classList.add("d-none");
+  prev.classList.remove("d-none");
+}
+
+function channelViewSwither(tvNav, channelsMenu) {
+  tvNav.classList.add("d-none");
+  channelsMenu.classList.remove("d-none");
+}
+
+function clearCheckedTVOptionsOnTooltipHide(elems) {
+  elems.forEach(active => {
+    active.classList.remove("active");
+  });
+}
+
+function handleMenuOptionsClick(options) {
+  options.forEach(option => {
+    option.addEventListener("click", () => {
+      option.classList.toggle("active");
+    });
+  });
+}
+
+function toggleMenuTooltip(el) {
+  el && el.addEventListener("click", () => {
+    el.classList.toggle("btn__active");
+  });
+  window.addEventListener('click', (e) => {
+    if (!el.contains(e.target)) {
+      el.classList.remove("btn__active");
+      const activeOptions = el.closest(".tv-nav__item").querySelectorAll(".active");
+      activeOptions && clearCheckedTVOptionsOnTooltipHide(activeOptions);
+    }
+  });
+}
 
 // svg sprites
 function requireAll(r) {
